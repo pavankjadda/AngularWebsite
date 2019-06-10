3c443d487d16865190d5ac4d7e1a0596187f4b94
Pavan Jadda <>
2019-06-10 17:27:23 -0400

    Updated Packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {LayoutModule} from './layouts/layout.module';
+import {HomeComponent} from './layouts/home/home.component';
+import {PageNotFoundComponent} from './layouts/pagenotfound/pagenotfound.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations:
+    [
+      AppComponent,
+      HomeComponent,
+      PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/layouts/pagenotfound/pagenotfound.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+
+@Component({
+  selector: 'app-pagenotfound',
+  templateUrl: './pagenotfound.component.html',
+  styleUrls: ['./pagenotfound.component.css']
+})
+export class PageNotFoundComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
