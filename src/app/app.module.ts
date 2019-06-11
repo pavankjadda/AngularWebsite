bc058f215115a6915b0cbde4455d3ccbf616ce78
Pavan Jadda <jpavanaryan@gmail.com>
2019-06-11 02:55:26 -0400

    Removed unised libraries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,15 @@ import {AppRoutingModule} from './app-routing.module';
 import {PagesModule} from './pages/pages.module';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
+import 'node_modules/startbootstrap-creative/vendor/jquery/jquery.slim.js';
+import 'node_modules/startbootstrap-creative/vendor/jquery-easing/jquery.easing.js';
+import 'node_modules/startbootstrap-creative/vendor/bootstrap/js/bootstrap.bundle.min.js';
+import 'node_modules/startbootstrap-creative/vendor/magnific-popup/jquery.magnific-popup.js';
+
+//import 'node_modules/startbootstrap-creative/js/creative.js';
+
+
+
 @NgModule({
   declarations:
     [
