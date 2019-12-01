eeca54be02748733406005eae96d44c0734df39d
Pavan Jadda <jpavanaryan@gmail.com>
2019-12-01 14:33:48 -0500

    Add deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 ng build --prod --output-path docs --base-href pavankjadda.github.io
 
-cp dist/AngularWebsite /Users/pjadda/WebstormProjects/pavankjadda.github.io/
+cp -Rf dist/AngularWebsite/ /Users/pjadda/WebstormProjects/pavankjadda.github.io/
