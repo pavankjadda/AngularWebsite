241feddf4c4b113fbca63b61f9de42522fde0748
Pavan Jadda <jpavanaryan@gmail.com>
2020-04-25 13:36:41 -0400

    Update .gitignore

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 ###
 
 echo "Building project ..."
-ng build --prod --output-path docs --base-href pavankjadda.github.io
+ng build --prod --output-path docs
 
 echo "Copying dist folder contents to pavankjadda.github.io project directory"
-cp -Rf dist/AngularWebsite/ /Users/pjadda/WebstormProjects/pavankjadda.github.io/
+cp -Rf "$HOME"/WebstormProjects/AngularWebsite/docs "$HOME"/WebstormProjects/pavankjadda.github.io/
 
 echo "Copy index.html to 404.html"
-cp /Users/pjadda/WebstormProjects/pavankjadda.github.io/index.html /Users/pjadda/WebstormProjects/pavankjadda.github.io/404.html
+cp "$HOME"/WebstormProjects/pavankjadda.github.io/index.html "$HOME"/WebstormProjects/pavankjadda.github.io/404.html
