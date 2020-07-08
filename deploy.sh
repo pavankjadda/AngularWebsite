6fc464a175369ab57988587d260ca1addd1ea6cb
Pavan K Jadda <pavankjadda@users.noreply.github.com>
2020-07-08 02:28:26 -0400

    Updates deployment script

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,14 @@
 echo "Building project ..."
 ng build --prod --output-path docs
 
-echo "Copying dist folder contents to pavankjadda.github.io project directory"
+echo "Copying dist folder contents to pavankjadda.github.io project directory..."
 cp -Rf "$HOME"/WebstormProjects/AngularWebsite/docs/ "$HOME"/WebstormProjects/pavankjadda.github.io/
+echo "Copied dist folder contents to pavankjadda.github.io project directory..."
 
-echo "Copy index.html to 404.html"
+echo "Coping index.html to 404.html..."
 cp "$HOME"/WebstormProjects/pavankjadda.github.io/index.html "$HOME"/WebstormProjects/pavankjadda.github.io/404.html
+echo "Copied index.html to 404.html"
+
+echo "Moving to pavankjadda.github.io project directory and committing code.."
+cd "$HOME"/WebstormProjects/pavankjadda.github.io/ && git add . && git commit -m "Latest Update" && git push origin master
+echo "Pushed code to pavankjadda.github.io Github"
